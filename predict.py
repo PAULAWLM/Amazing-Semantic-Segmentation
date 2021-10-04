7d48b2a3fa4282d324fd6706e71ba94125bce190
PAULAWLM <paulawlm@icloud.com>
2021-10-04 14:13:15 +0200

    + .args

--- predict.py
+++ predict.py
@@ -53,12 +53,12 @@ if not os.path.exists(args.image_path):
 from model import Deeplabv3
 net = Deeplabv3(weights='pascal_voc', input_tensor=None, input_shape=(512, 512, 3), classes=2, backbone='mobilenetv2',
               OS=8, alpha=1., activation=None)
-              
+
 # load weights
 print('Loading the weights...')
 if args.weights is None:
     net.load_weights(filepath=os.path.join(
-        paths['weigths_path'], '{model}_based_on_{base_model}.h5'.format(model=args.model, base_model=base_model)))
+        paths['weigths_path'], '{model}_based_on_{base_model}.h5'.format(model=args.model, base_model=args.base_model)))
 else:
     if not os.path.exists(args.weights):
         raise ValueError('The weights file does not exist in \'{path}\''.format(path=args.weights))
@@ -67,7 +67,7 @@ else:
 # begin testing
 print("\n***** Begin testing *****")
 print("Model -->", args.model)
-print("Base Model -->", base_model)
+print("Base Model -->", args.base_model)
 print("Crop Height -->", args.crop_height)
 print("Crop Width -->", args.crop_width)
 print("Num Classes -->", args.num_classes)
